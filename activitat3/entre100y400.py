7333da01292670377a56ac38d955eccfb41d648a
sikoz <izan.mediel@gmail.com>
2024-11-20 21:33:18 +0100

    Update entre100y400.py

--- activitat3/entre100y400.py
+++ activitat3/entre100y400.py
@@ -1,7 +1,7 @@
 numero = int(input("introduce un numero: "))
 
 while numero < 100 or numero > 400:
-    print("El numero no esta entre 100 y 4002")
+    print("El numero no esta entre 100 y 400")
     numero = int(input("introduce otro numero: "))
 
 print(f"El numero esta entre 100 y 400")
